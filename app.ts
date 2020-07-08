51be3fedbc38c0cdc13893899becaeb155704491
Billi Ramirez <ramirezbilly42@outlook.com>
2020-07-08 14:25:21 -0600

    array types

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
-const person: { name: string; age: number } = {
+const person = {
   name: "Billi",
   age: 22,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sport"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
