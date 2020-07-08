51c1a32fa2e6c6f8f27567c0c1069c6463773652
Billi Ramirez <ramirezbilly42@outlook.com>
2020-07-08 14:19:18 -0600

    object types

--- app.ts
+++ app.ts
@@ -1,13 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  if (showResult) {
-    console.log(`${phrase} ${n1 + n2}`);
-  }
-  return n1 + n2;
-}
+const person: { name: string; age: number } = {
+  name: "Billi",
+  age: 22,
+};
 
-const number1 = 5;
-const number2 = 4.4;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
