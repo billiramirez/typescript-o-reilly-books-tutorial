74030c28480592c1702e862e58e5c603297d2662
Billi Ramirez <ramirezbilly42@outlook.com>
2020-07-09 11:22:49 -0600

    never

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 4;
+userInput = "Max";
+/**
+ * unknown: If you want to use this type, you should be extremly sure you don't know
+ * the type of the value. You cannot asign a unkown value to another type without runtime
+ * type checking.
+ *
+ * any: this is more flexible
+ */
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
+function generateError(message, code) {
+    throw {
+        message: message,
+        errorCode: code
+    };
 }
-printResult(add(1, 2));
+generateError("An error ocurred", 500);
--- app.ts
+++ app.ts
@@ -15,3 +15,12 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw {
+    message,
+    errorCode: code,
+  };
+}
+
+generateError("An error ocurred", 500);
