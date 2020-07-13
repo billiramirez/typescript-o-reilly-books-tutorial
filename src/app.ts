b55507ac3a20c50521a04c422fd3d1adc858a4eb
Billi Ramirez <ramirezbilly42@outlook.com>
2020-07-13 11:34:51 -0600

    private and public access modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accounting = new Department("Accounting");
 console.log(accounting);
+
+accounting.addEmployee("Billi");
+accounting.addEmployee("Alexander");
+
+accounting.printEmployeeInformation();
+
+accounting.describe();
