6033655cf0b6792b639538b25ecc350cf98ac89f
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-01 14:43:49 -0600

    partials and readonly

--- src/app.ts
+++ src/app.ts
@@ -49,3 +49,26 @@ textStorage.removeItem("Billi");
 console.log(textStorage.getItems());
 
 const numberStorage = new DataStorage<number>();
+
+// Adding Partials
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Alex"];
+// names.push("hee");
+// names.pop();
