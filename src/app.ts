bf2af7c69ca00ea45d328c37033ee27706dfb087
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-01 11:36:55 -0600

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,29 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "billi" }, "name");
+
+// Generic Classes
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Billi");
+textStorage.addItem("Ramirez");
+textStorage.removeItem("Billi");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
