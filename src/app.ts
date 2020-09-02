4b3143801bd02e80a2ae5c427e25fba177618beb
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-02 09:13:59 -0600

    my first decorator

--- src/app.ts
+++ src/app.ts
@@ -1,74 +1,16 @@
-interface Lengthy {
-  length: number;
+function Logger(constructor: Function) {
+  console.log("loggin");
+  console.log(constructor);
 }
 
-function countAndPrint<T extends Lengthy>(element: T): [T, string] {
-  let description = " go no value";
-  if (element.length === 1) {
-    description = "Got 1 Element";
-  } else if (element.length > 1) {
-    description = "Got " + element.length + "elements.";
-  }
-  return [element, description];
-}
+@Logger
+class Person {
+  name = "billi";
 
-console.log(countAndPrint("haha")[1]);
-
-// Using the keyof
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return "Value " + obj[key];
-}
-
-extractAndConvert({ name: "billi" }, "name");
-
-// Generic Classes
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
+  constructor() {
+    console.log("creating person object");
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem("Billi");
-textStorage.addItem("Ramirez");
-textStorage.removeItem("Billi");
-console.log(textStorage.getItems());
-
-const numberStorage = new DataStorage<number>();
-
-// Adding Partials
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
-}
-
-function createCourseGoal(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ["Max", "Alex"];
-// names.push("hee");
-// names.pop();
+const pers = new Person();
+console.log(pers);
