b31b64eb28251112e62bd11c83eaafc84ddc447b
Billi Ramirez <ramirezbilly42@outlook.com>
2020-09-02 10:10:01 -0600

    adding multiple decorators

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
-function Logger(constructor: Function) {
-  console.log("loggin");
-  console.log(constructor);
+function Logger(logString: string) {
+  console.log("LOGGER DECORATOR");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE DECORATOR");
+
+  return function (constructor: any) {
+    console.log("rendering template");
+
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Loggin - Person")
+@Logger("Loggin")
+@WithTemplate("<h1>My Person Object</>", "app")
 class Person {
   name = "billi";
-
   constructor() {
     console.log("creating person object");
   }
